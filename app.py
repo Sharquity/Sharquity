77f6ec272c992b94dfc6b9440478dc919f321c04
Suufi <qxest@reqxest.com>
2018-12-02 22:16:07 -0500

    Implement / endpoint

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 #from flask_restful import Resource, Api
+from flask_cors import CORS, cross_origin
+from neural_net import NeuralNet
+
+
+net = NeuralNet()
+net.init_model()
 
 app = Flask(__name__)
 #api = Api(app)
 
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 company_factors = {}
 
 @app.route('/', methods=['GET', 'POST'])
+@cross_origin()
 def main():
     import json
-    return ''' hell word '''
+    body = request.get_json()
+    print(body)
+    return jsonify(net.predictOffer((body['askedFor'], body['exchangeForStake'], body['valuation'], body['industry'])))
 
 @app.route('/eval', methods=['POST'])
 def evalution():
